7710cabafd00d7f257bd947adb38d4262538e224
Daniel Lemire <lemire@gmail.com>
2015-09-30 21:11:49 -0400

    Preparing new version.

--- README.md
+++ README.md
@@ -59,6 +59,7 @@ Is it faster?
 It tends to fare well against the competition :
 
 ```
+$ node test.js
 Platform: linux 3.13.0-37-generic x64
 Intel(R) Core(TM) i7-4770 CPU @ 3.40GHz
 Node version 0.12.7, v8 version 3.28.71.19
@@ -66,22 +67,25 @@ Node version 0.12.7, v8 version 3.28.71.19
 Comparing againsts:
 js-priority-queue: https://github.com/adamhooper/js-priority-queue
 heap.js: https://github.com/qiao/heap.js
+binaryheapx: https://github.com/xudafeng/BinaryHeap
+priority_queue: https://github.com/agnat/js_priority_queue
+js-heap: https://github.com/thauburger/js-heap
 
 starting dynamic queue/enqueue benchmark
-FastPriorityQueue x 7,341 ops/sec ±0.14% (103 runs sampled)
-js-priority-queue x 4,908 ops/sec ±0.24% (102 runs sampled)
-heap.js x 6,518 ops/sec ±0.06% (103 runs sampled)
+FastPriorityQueue x 7,353 ops/sec ±0.06% (101 runs sampled)
+js-priority-queue x 4,905 ops/sec ±0.20% (102 runs sampled)
+heap.js x 6,576 ops/sec ±0.18% (102 runs sampled)
+binaryheapx x 4,344 ops/sec ±0.21% (102 runs sampled)
+priority_queue x 3,357 ops/sec ±0.86% (100 runs sampled)
+js-heap x 499 ops/sec ±0.34% (96 runs sampled)
 Fastest is FastPriorityQueue
 
 starting dynamic tiny queue/enqueue benchmark
-FastPriorityQueue x 14,233 ops/sec ±0.13% (104 runs sampled)
-js-priority-queue x 11,011 ops/sec ±0.34% (103 runs sampled)
-heap.js x 11,027 ops/sec ±0.09% (104 runs sampled)
-Fastest is FastPriorityQueue
-
-starting dynamic huge queue/enqueue benchmark
-FastPriorityQueue x 1,776 ops/sec ±0.16% (101 runs sampled)
-js-priority-queue x 1,346 ops/sec ±0.14% (102 runs sampled)
-heap.js x 1,678 ops/sec ±0.20% (101 runs sampled)
+FastPriorityQueue x 16,037 ops/sec ±0.18% (104 runs sampled)
+js-priority-queue x 10,987 ops/sec ±0.52% (103 runs sampled)
+heap.js x 10,787 ops/sec ±0.12% (104 runs sampled)
+binaryheapx x 9,671 ops/sec ±0.41% (103 runs sampled)
+priority_queue x 4,828 ops/sec ±6.99% (91 runs sampled)
+js-heap x 991 ops/sec ±0.36% (100 runs sampled)
 Fastest is FastPriorityQueue
 ```
--- benchmark/test.js
+++ benchmark/test.js
@@ -104,98 +104,6 @@ var b = new PriorityQueue({ comparator: function(a, b) {
 
 
 
-
-function HugeQueueEnqueueBench() {
-    console.log("starting dynamic huge queue/enqueue benchmark");
-    var suite = new Benchmark.Suite();
-    // add tests
-    var ms = suite.add('FastPriorityQueue', function() {
-        var b = new FastPriorityQueue(function(a, b) {
-            return a - b;
-        });
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.add((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.add((33*i+5)%1024);
-            b.poll();
-        }
-        return b;
-    }  )
-
-    .add('js-priority-queue', function() {
-var b = new PriorityQueue({ comparator: function(a, b) {
-            return b - a;
-        }
-                                  });
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.queue((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.queue((33*i+5)%1024);
-            b.dequeue();
-        }
-        return b;
-    }  )
-    .add('heap.js', function() {
-        var b = new Heap(function(a, b) {
-            return a - b;
-        });
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.push((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.push((33*i+5)%1024);
-            b.pop();
-        }
-        return b;
-    }  )
-    .add('binaryheapx', function() {
-        var b = new binaryheapx();
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.add((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.add((33*i+5)%1024);
-            b.pop();
-        }
-        return b;
-    }  )
-    .add('priority_queue', function() {
-        var b = new pq.PriorityQueue();
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.push((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.push((33*i+5)%1024);
-            b.shift();
-        }
-        return b;
-    }  )
-    .add('js-heap', function() {
-        var b = new jsHeap();
-        for(var i = 0 ; i < 12000  ; i++) {
-            b.push((33*i+5)%1024);
-        }
-        for(i = 128 ; i < 128 * 10  ; i++) {
-            b.push((33*i+5)%1024);
-            b.pop();
-        }
-        return b;
-    }  )
-    // add listeners
-    .on('cycle', function(event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function() {
-        console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-    })
-    // run async
-.run({ 'async': false });
-}
-
-
-
 function TinyQueueEnqueueBench() {
     console.log("starting dynamic tiny queue/enqueue benchmark");
     var suite = new Benchmark.Suite();
@@ -293,7 +201,7 @@ var main = function() {
     console.log(os.cpus()[0]["model"]);
     console.log("Node version "+process.versions.node+", v8 version "+process.versions.v8);
     console.log();
-    console.log("Comparing againsts: ");
+    console.log("Comparing against: ");
     console.log("js-priority-queue: https://github.com/adamhooper/js-priority-queue");
     console.log("heap.js: https://github.com/qiao/heap.js");
     console.log("binaryheapx: https://github.com/xudafeng/BinaryHeap");
@@ -304,7 +212,6 @@ var main = function() {
     console.log("");
     TinyQueueEnqueueBench();
     console.log("");
-    HugeQueueEnqueueBench();
 }
 
 if (require.main === module) {
